6c22d0b4b74c118605d70c5d811c8700c9158c65
Martin Ireblom <martin@ireblom.se>
2019-04-01 19:23:55 +0200

    💡 Add usage (--help)

--- Sources/whenami/main.swift
+++ Sources/whenami/main.swift
@@ -1,1 +1,30 @@
+import Foundation
+
+let arguments = CommandLine.arguments
+if arguments.count > 1 {
+    let executable = URL(fileURLWithPath: arguments[0]).lastPathComponent
+    let usage = "usage: \(executable) [-h]"
+
+    if arguments.count == 2 {
+        let argument = arguments[1].trimmingCharacters(in: .whitespacesAndNewlines)
+
+        if argument == "-h" || argument == "--help" {
+            print(usage);
+            print()
+            print("optional arguments:")
+            print("  -h, --help  show this help message and exit")
+            exit(EXIT_SUCCESS)
+        }
+    }
+
+    let userName = NSUserName()
+    let apology = "I'm sorry \(userName). I'm afraid I can't do that."
+    let invalidArguments = arguments.joined(separator:" ")
+    let errorMessage = "\(usage)\n\(apology)\n  \(invalidArguments)\n"
+    let standardError = FileHandle.standardError
+    standardError.write(errorMessage.data(using: .utf8)!)
+    exit(EXIT_FAILURE)
+}
+
 print("now")
+exit(EXIT_SUCCESS)
