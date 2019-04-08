b9812580bd29249a6cda9fd8d396f9b9aba6db2c
Martin Ireblom <martin@ireblom.se>
2019-04-08 20:19:22 +0200

    🛁 Make swiftlint happy

--- Sources/whenami/main.swift
+++ Sources/whenami/main.swift
@@ -1,16 +1,17 @@
 import Foundation
 
-let version = "4.1.2019"
-let arguments = CommandLine.arguments
-if arguments.count > 1 {
-    let executable = URL(fileURLWithPath: arguments[0]).lastPathComponent
+let kVersion = "4.1.2019"
+
+if CommandLine.arguments.count > 1 {
+    let executable = URL(fileURLWithPath: CommandLine.arguments[0]).lastPathComponent
     let usage = "usage: \(executable) [-h] [-v]"
-    
-    if arguments.count == 2 {
-        let argument = arguments[1].trimmingCharacters(in: .whitespacesAndNewlines)
-        
+
+    let arguments = CommandLine.arguments[1...CommandLine.arguments.count - 1]
+    if arguments.count == 1 {
+        let argument = arguments.first?.trimmingCharacters(in: .whitespacesAndNewlines)
+
         if argument == "-h" || argument == "--help" {
-            print(usage);
+            print(usage)
             print()
             print("optional arguments:")
             print("  -h, --help     show this help message and exit")
@@ -18,14 +19,14 @@ if arguments.count > 1 {
             exit(EXIT_SUCCESS)
         }
         if argument == "-v" || argument == "--version" {
-            print(version)
+            print(kVersion)
             exit(EXIT_SUCCESS)
         }
     }
-    
+
     let userName = NSUserName()
     let apology = "I'm sorry \(userName). I'm afraid I can't do that."
-    let invalidArguments = arguments[1...arguments.count-1].joined(separator:" ")
+    let invalidArguments = arguments.joined(separator: " ")
     let errorMessage = "\(usage)\n\(apology)\n  \(invalidArguments)\n"
     let standardError = FileHandle.standardError
     standardError.write(errorMessage.data(using: .utf8)!)
--- Tests/whenamiTests/WhenamiTests.swift
+++ Tests/whenamiTests/WhenamiTests.swift
@@ -1,126 +1,127 @@
 import XCTest
 import class Foundation.Bundle
 
-final class whenamiTests: XCTestCase {
+final class WhenamiTests: XCTestCase {
     func testNoArgs() throws {
         // Some of the APIs that we use below are available in macOS 10.13 and above.
         guard #available(macOS 10.13, *) else {
             return
         }
-        
+
         let whenamiBinary = productsDirectory.appendingPathComponent("whenami")
-        
+
         let process = Process()
         process.executableURL = whenamiBinary
-        
+
         let outPipe = Pipe()
         process.standardOutput = outPipe
         let errPipe = Pipe()
         process.standardError = errPipe
-        
+
         try process.run()
         process.waitUntilExit()
-        
+
         XCTAssertEqual(process.terminationStatus, 0)
-        
+
         let outData = outPipe.fileHandleForReading.readDataToEndOfFile()
         let output = String(data: outData, encoding: .utf8)
         let expectedOutput = "now\n"
         XCTAssertEqual(output, expectedOutput)
-        
+
         let errData = errPipe.fileHandleForReading.readDataToEndOfFile()
         let error = String(data: errData, encoding: .utf8)
         let expectedError = ""
         XCTAssertEqual(error, expectedError)
     }
-    
+
     func testHelpArgs() throws {
         // Some of the APIs that we use below are available in macOS 10.13 and above.
         guard #available(macOS 10.13, *) else {
             return
         }
-        
+
         let whenamiBinary = productsDirectory.appendingPathComponent("whenami")
-        
+
         for helpArgument in ["-h", "--help"] {
             let process = Process()
             process.executableURL = whenamiBinary
-            
+
             let arguments = [helpArgument]
             process.arguments = arguments
             let outPipe = Pipe()
             process.standardOutput = outPipe
             let errPipe = Pipe()
             process.standardError = errPipe
-            
+
             try process.run()
             process.waitUntilExit()
-            
+
             XCTAssertEqual(process.terminationStatus, 0)
-            
+
             let outData = outPipe.fileHandleForReading.readDataToEndOfFile()
             let output = String(data: outData, encoding: .utf8)
+            let intentionallyLeftBlank = ""
             let expectedOutput = """
                                  usage: whenami [-h] [-v]
-                                 
+                                 \(intentionallyLeftBlank)
                                  optional arguments:
                                    -h, --help     show this help message and exit
                                    -v, --version  show version number and exit
-                                 
+                                 \(intentionallyLeftBlank)
                                  """
             XCTAssertEqual(output, expectedOutput)
-            
+
             let errData = errPipe.fileHandleForReading.readDataToEndOfFile()
             let error = String(data: errData, encoding: .utf8)
             let expectedError = ""
             XCTAssertEqual(error, expectedError)
         }
     }
-    
+
     func testVersionArgs() throws {
         // Some of the APIs that we use below are available in macOS 10.13 and above.
         guard #available(macOS 10.13, *) else {
             return
         }
-        
+
         let whenamiBinary = productsDirectory.appendingPathComponent("whenami")
-        
+
         for versionArgument in ["-v", "--version"] {
             let process = Process()
             process.executableURL = whenamiBinary
-            
+
             let arguments = [versionArgument]
             process.arguments = arguments
             let outPipe = Pipe()
             process.standardOutput = outPipe
             let errPipe = Pipe()
             process.standardError = errPipe
-            
+
             try process.run()
             process.waitUntilExit()
-            
+
             XCTAssertEqual(process.terminationStatus, 0)
-            
+
             let outData = outPipe.fileHandleForReading.readDataToEndOfFile()
             let output = String(data: outData, encoding: .utf8)
             let expectedOutput = "4.1.2019\n"
             XCTAssertEqual(output, expectedOutput)
-            
+
             let errData = errPipe.fileHandleForReading.readDataToEndOfFile()
             let error = String(data: errData, encoding: .utf8)
             let expectedError = ""
             XCTAssertEqual(error, expectedError)
         }
     }
-    
+
     func testWrongArgs() throws {
         // Some of the APIs that we use below are available in macOS 10.13 and above.
         guard #available(macOS 10.13, *) else {
             return
         }
-        
+
         let whenamiBinary = productsDirectory.appendingPathComponent("whenami")
-        
+
         let process = Process()
         process.executableURL = whenamiBinary
         let arguments = ["I am sooo invalid!"]
@@ -129,29 +130,30 @@ final class whenamiTests: XCTestCase {
         process.standardOutput = outPipe
         let errPipe = Pipe()
         process.standardError = errPipe
-        
+
         try process.run()
         process.waitUntilExit()
-        
+
         XCTAssertEqual(process.terminationStatus, 1)
-        
+
         let outData = outPipe.fileHandleForReading.readDataToEndOfFile()
         let output = String(data: outData, encoding: .utf8)
         let expectedOutput = ""
         XCTAssertEqual(output, expectedOutput)
-        
+
         let errData = errPipe.fileHandleForReading.readDataToEndOfFile()
         let error = String(data: errData, encoding: .utf8)
         let userName = NSUserName()
+        let intentionallyLeftBlank = ""
         let expectedError = """
                             usage: whenami [-h] [-v]
                             I'm sorry \(userName). I'm afraid I can't do that.
                               \(arguments[0])
-                            
+                            \(intentionallyLeftBlank)
                             """
         XCTAssertEqual(error, expectedError)
     }
-    
+
     /// Returns path to the built products directory.
     var productsDirectory: URL {
       #if os(macOS)
@@ -163,7 +165,7 @@ final class whenamiTests: XCTestCase {
         return Bundle.main.bundleURL
       #endif
     }
-    
+
     static var allTests = [
         ("testNoArgs", testNoArgs),
         ("testHelpArgs", testHelpArgs),
